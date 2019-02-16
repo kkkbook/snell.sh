9b0c6455d22fbaf1865b81233f6a542f274ddd01
XZChen <2242315+primovist@users.noreply.github.com>
2019-02-16 22:29:04 +0800

    Create snell.sh
    初始版本，适用于x64构架Linux

---
+++ snell.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+apt-get install unzip wget
+cd ～/
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/1.0/snell-server-v1.0.0-linux-amd64.zip
+unzip snell.zip
+rm snell.zip -y
+chmod 777 snell-server
+cd /etc/systemd/system
+
+cat > snell.service<<-EOF
+[Unit]
+Description=Snell Server
+After=network.target
+
+[Service]
+ExecStart=/root/snell-server
+Restart=on-failure
+RestartSec=1s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl start snell
+systemctl restart snell
+cat /snell-server.conf
