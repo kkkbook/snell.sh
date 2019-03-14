25652148858d5dfcdb9d31bfc9b335c4443c1c1c
primovist <chenxiaozhuang@outlook.com>
2019-03-14 10:55:35 +0800

    更新&添加
    修改ubuntu&debian安装方式，添加centos&redhat安装方式

--- snell.centos.sh
+++ snell.centos.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
-apt-get install unzip -y
+yum install unzip -y
 cd ~/
 wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/v1.1.0/snell-server-v1.1.0-linux-amd64.zip
 unzip -o snell.zip
