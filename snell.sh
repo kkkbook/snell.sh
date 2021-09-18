d8257321b7cb1854651626dc0bf5c5603d6fb2d0
XZChen <xzchen@xzchen.pro>
2021-09-18 14:15:24 +0800

    Update snell.sh

--- snell.sh
+++ snell.sh
@@ -5,7 +5,7 @@ CONF="/etc/snell/snell-server.conf"
 SYSTEMD="/etc/systemd/system/snell.service"
 apt-get install unzip -y
 cd ~/
-wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/2.0.5b3/snell-server-v2.0.5-linux-amd64.zip
+wget --no-check-certificate -O snell.zip https://github.com/surge-networks/snell/releases/download/2.0.5/snell-server-v2.0.5-linux-amd64.zip
 unzip -o snell.zip
 rm -f snell.zip
 chmod +x snell-server
